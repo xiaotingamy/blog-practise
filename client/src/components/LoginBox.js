7251b39b7e361ccf5982f87bf595556a5877726d
xiaotingamy <502150736@qq.com>
2016-12-14 17:31:22 +0800

    replace ajax with fetch

--- client/src/components/LoginBox.js
+++ client/src/components/LoginBox.js
@@ -1,21 +1,46 @@
 import React from 'react'
 import LoginForm from './LoginForm'
-import $ from 'jquery'
+
+function status(response) {
+    if (response.status >= 200 && response.status <300) {
+        return response
+    }
+    throw new Error(response.statusText);
+}
+
+function json(response) {
+    return response.json()
+}
 
 export default React.createClass( {
     handleLoginSubmit(logindata) {
-      $.ajax({
-          url: this.props.url,
-          datatype: 'json',
-          type: 'post',
-          data: logindata,
-          success: function (data) {
-              alert('success')
-          }.bind(this),
-          error: function (xhr, status, err) {
-              alert(err);
-          }.bind(this)
-      })
+        // $.ajax({
+        //     url: this.props.url,
+        //     datatype: 'json',
+        //     type: 'post',
+        //     data: logindata,
+        //     success: function (data) {
+        //         alert('success')
+        //     }.bind(this),
+        //     error: function (xhr, status, err) {
+        //         alert(err);
+        //     }.bind(this)
+        // })
+        var req = new Request(this.props.url, {
+            method: 'POST',
+            headers: {
+                    'Content-Type': 'application/x-www-form-urlencoded'
+                }, 
+            body: logindata
+        });
+        fetch(req)
+        .then(status)
+        .then(json)
+        .then(function(json){
+            alert('Request success');
+        }).catch(function(error){
+            alert(error)
+        })
     },
     render() {
         return (
